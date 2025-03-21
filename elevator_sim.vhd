6bc8938827e37dcaecc1ae9e9b30aa18d3f0948c
Nolan Larison <larison.15@buckeyemail.osu.edu>
2025-03-21 12:12:55 -0400

    added ports and behavior

--- elevator_sim.vhd
+++ elevator_sim.vhd
@@ -4,8 +4,23 @@ use ieee.std_logic_1164.all;
 
 entity elevator_sim is
   port(
-
+  poc : in std_logic;
+  clk : in std_logic;
+  emvup : in std_logic;
+  emvdn : in std_logic;
+  eopen : in std_logic;
+  eclose : in std_logic;
+  ecomp : out std_logic;
+  ef : out std_logic_vector(4 downto 1)
       );
-
-
 end elevator_sim;
+
+
+architecture behavior of elevator_sim is
+
+
+begin
+
+
+
+end behavior;
